78618970d395c5c4e86107c15a59b23b008e8fb6
william <williamnnmiranda@gmail.com>
2022-02-25 20:11:52 -0300

    name bd

--- src/database/db.js
+++ src/database/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://williamnm:will2812@cluster0.tbjnq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://williamnm:will2812@cluster0.tbjnq.mongodb.net/Cluster0?retryWrites=true&w=majority', {
     useNewUrlParser : true, 
     useUnifiedTopology : true}).then(() => console.log("Conectado ao mongo")).catch((err) => console.error(err))
